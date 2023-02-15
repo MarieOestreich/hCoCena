a5efbca533b642181b54df56b13b8bb786e38400
Marie Oestreich <marie.oestreich@gmx.de>
2023-02-15 10:58:14 +0100

    'propr'cannot be intalled via cran anymore, only via github.

--- install_hcocena.R
+++ install_hcocena.R
@@ -1,1 +1,2 @@
+devtools::install_github("tpq/propr")
 remotes::install_url(url="https://github.com/MarieOestreich/hCoCena/archive/refs/heads/main.zip", subdir="hCoCena-r-package", dependencies=TRUE, upgrade="never")
