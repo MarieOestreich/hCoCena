2eaf226f60040294e472805b2ac5a0a9e821298f
Marie Oestreich <marie.oestreich@gmx.de>
2023-04-12 17:03:56 +0200

    Update install_hcocena.R
    added 'remotes' installation to address #5

--- install_hcocena.R
+++ install_hcocena.R
@@ -1,2 +1,3 @@
-devtools::install_github("tpq/propr")
+install.packages('remotes')
+remotes::install_github("tpq/propr")
 remotes::install_url(url="https://github.com/MarieOestreich/hCoCena/archive/refs/heads/main.zip", subdir="hCoCena-r-package", dependencies=TRUE, upgrade="never")
