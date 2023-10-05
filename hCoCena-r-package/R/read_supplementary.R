2aafcf459888b373a595433c5fa36951f729501d
Dahm <dahmk@dzne.ds>
2023-10-05 08:50:05 +0200

    Profiling functions substituted with enrich_module function and modifications to the supplement file read-in

--- hCoCena-r-package/R/read_supplementary.R
+++ hCoCena-r-package/R/read_supplementary.R
@@ -3,15 +3,21 @@
 #' Function to read and collect the supplementary files set in set_supp_files().
 #' @export
 
-read_supplementary <- function(){
-  
-  
-  hcobject[["supplementary_data"]][["TF"]] <<- utils::read.delim(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], hcobject[["supplement"]][1]),
-                             header=TRUE,
-                             check.names=F)
-  
-  hcobject[["supplementary_data"]][["hallmark"]] <<- clusterProfiler::read.gmt(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], hcobject[["supplement"]][2]))
-  
-  hcobject[["supplementary_data"]][["go"]] <<- clusterProfiler::read.gmt(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], hcobject[["supplement"]][3]))
-  
+read_supplementary <- function () 
+{
+  if(!is.null(Tf))
+    hcobject[["supplementary_data"]][["TF"]] <<- utils::read.delim(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], 
+                                                                                hcobject[["supplement"]][1]), header = TRUE, check.names = F)
+  if(!is.null(Hall))
+    hcobject[["supplementary_data"]][["hallmark"]] <<- clusterProfiler::read.gmt(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], 
+                                                                                              hcobject[["supplement"]][2]))
+  if(!is.null(Go))
+    hcobject[["supplementary_data"]][["go"]] <<- clusterProfiler::read.gmt(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], 
+                                                                                        hcobject[["supplement"]][3]))
+  if(!is.null(Kegg))
+    hcobject[["supplementary_data"]][["Kegg"]] <<- clusterProfiler::read.gmt(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], 
+                                                                                          hcobject[["supplement"]][4]))
+  if(!is.null(Reactome))
+    hcobject[["supplementary_data"]][["Reactome"]] <<- clusterProfiler::read.gmt(base::paste0(hcobject[["working_directory"]][["dir_reference_files"]], 
+                                                                                              hcobject[["supplement"]][5]))
 }
--- hCoCena-r-package/R/set_supp_files.R
+++ hCoCena-r-package/R/set_supp_files.R
@@ -3,12 +3,13 @@
 #' Receives the file names of the supplementary files
 #' @param Tf A file name. The file must contain a list of at least two columns. One column should be titled with the organism from which the datasets originate and contain gene names, the other column must always be the last column and contain the information if the gene is a transcription factor ("TF"),
 #' 	a co-factor ("Co_factor"), a chromatin remodelling protein ("Chromatin_remodeller") or a ribonucleic acid binding protein ("RNBP"). The name of the last column may vary. An exmemplary file for mouse and human is found in the Reference Files Folder in the repository.
-#' @param Hall A file name. DESCRIBE ONLY IF NECESSARY.
+#' @param Hall A file name for a .gmt MSigDB hallmark gene set file. You can find one in the reference file folder in the repository.
 #' @param Go A file name for a .gmt gene ontology file. You can find one in the reference file folder in the repository.
+#' @param Kegg A file name for a .gmt KEGG database file. You can find one in the reference file folder in the repository.
+#' @param Reactome A file name for a .gmt Reactomte database file. You can find one in the reference file folder in the repository.
 #' @export
 
-set_supp_files <- function(Tf, Hall, Go){
-
-	hcobject[["supplement"]] <<- base::c(Tf, Hall, Go)
-
+set_supp_files <- function (Tf = NULL, Hall = NULL, Go = NULL, Kegg = NULL, Reactome = NULL) 
+{
+  hcobject[["supplement"]] <<- base::c(Tf, Hall, Go, Kegg, Reactome)
 }
