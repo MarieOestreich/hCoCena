48c8c5ca4fddcc720a04003d0d5051ddaf2d9aff
LisaHolsten <lisaholsten@gmx.de>
2023-10-09 15:20:48 +0200

    MCDA removed from CRAN. Installation of version 0.0.24.

--- install_hcocena.R
+++ install_hcocena.R
@@ -1,3 +1,4 @@
 install.packages('remotes')
 remotes::install_github("tpq/propr")
+remotes::install_url(url = "https://cran.r-project.org/src/contrib/Archive/MCDA/MCDA_0.0.24.tar.gz")
 remotes::install_url(url="https://github.com/MarieOestreich/hCoCena/archive/refs/heads/main.zip", subdir="hCoCena-r-package", dependencies=TRUE, upgrade="never")
